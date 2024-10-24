2d1da8425acc392d93e095a74dd677be8641c45d
fredlesieur <fred.lesieur@hotmail.fr>
2024-10-24 18:52:51 +0200

    ok

--- Public/assets/javascript/compteur.js
+++ Public/assets/javascript/compteur.js
@@ -1,30 +1,27 @@
-document.querySelectorAll('.btn-info').forEach(button => {
-    button.addEventListener('click', function(event) {
+document.querySelectorAll('.btn-info').forEach(card => {
+    card.addEventListener('click', function(event) {
         event.preventDefault(); // Empêche la redirection immédiate
 
-        const animalId = this.getAttribute('href').split('/').pop(); // Récupère l'ID de l'animal à partir de l'URL
-        const animalLink = this.getAttribute('href'); // Récupère le lien de la fiche de l'animal
+        const animalId = this.dataset.animalId;
+        const animalLink = this.querySelector('.button').getAttribute('href'); // Récupère le lien de la fiche de l'animal
 
         // Envoi de la requête AJAX pour incrémenter le compteur de vues
         fetch(`/animal/incrementViews/${animalId}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-            }
+            },
+            body: JSON.stringify({ id: animalId }),
         })
         .then(response => response.json())
         .then(data => {
-            if (data.success) {
-                console.log('Compteur de vues mis à jour avec succès', data);
-                // Redirection vers la fiche de l'animal après mise à jour du compteur
-                window.location.href = animalLink;
-            } else {
-                console.error('Erreur lors de la mise à jour du compteur de vues', data.message);
-                window.location.href = animalLink;
-            }
+            console.log('Compteur de vues mis à jour avec succès', data);
+            // Redirection vers la fiche de l'animal après mise à jour du compteur
+            window.location.href = animalLink;
         })
         .catch(error => {
             console.error('Erreur lors de la mise à jour du compteur de vues', error);
+            // En cas d'erreur, redirection vers la fiche de l'animal
             window.location.href = animalLink;
         });
     });
