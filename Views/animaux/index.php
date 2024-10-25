7cea6445db6c88d152c0a04d0cb60d2eb593d679
fredlesieur <fred.lesieur@hotmail.fr>
2024-10-25 11:17:11 +0200

    ok

--- Views/animaux/index.php
+++ Views/animaux/index.php
@@ -4,28 +4,28 @@
     <div class="row justify-content-center pb-2">
         <div class="col-md-6 h-100">
             <div class="card h-100">
-                <img src="/assets/images/<?= $animal['animal_image'] ?>" class="card-img-top img-fluid2" alt="<?= $animal['animal_nom']; ?>" loading="lazy">
+                <img src="/assets/images/<?= isset($animal['animal_image']) ? htmlspecialchars($animal['animal_image']) : 'default.jpg'; ?>" class="card-img-top img-fluid2" alt="<?= isset($animal['animal_nom']) ? htmlspecialchars($animal['animal_nom']) : 'Nom inconnu'; ?>" loading="lazy">
                 <div class="card-body">
-                    <h3 class="card-title text-center"><?= ucfirst($animal['animal_nom']); ?></h3>
-                    <h4 class="card-text text-center"><strong>Âge : </strong> <?= ucfirst($animal['animal_age']); ?> ans</h4>
-                    <h4 class="card-text text-center"><strong>Race : </strong> <?= ucfirst($animal['animal_race']); ?></h4>
-                    <h4 class="card-text text-center"><strong>Habitat : </strong> <?= ucfirst($animal['animal_habitat']); ?></h4><br>
+                    <h3 class="card-title text-center"><?= isset($animal['animal_nom']) ? ucfirst(htmlspecialchars($animal['animal_nom'])) : 'Nom inconnu'; ?></h3>
+                    <h4 class="card-text text-center"><strong>Âge : </strong> <?= isset($animal['animal_age']) ? htmlspecialchars($animal['animal_age']) . ' ans' : 'Âge inconnu'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Race : </strong> <?= isset($animal['animal_race']) ? ucfirst(htmlspecialchars($animal['animal_race'])) : 'Race inconnue'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Habitat : </strong> <?= isset($animal['animal_habitat']) ? ucfirst(htmlspecialchars($animal['animal_habitat'])) : 'Habitat inconnu'; ?></h4><br>
                 </div>
             </div>
         </div>
     </div>
+
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
-                <h3 class="card-title text-center">Rapport Vétérinaire </h3>
-<h4 class="card-text text-center"><strong>Rapport de : <br> </strong> <?= isset($animal['user_nom_prenom']) ? ucfirst($animal['user_nom_prenom']) : 'Information non disponible'; ?></h4>
-<h4 class="card-text text-center"><strong>Etat : <br> </strong> <?= isset($animal['etat']) ? ucfirst($animal['etat']) : 'Information non disponible'; ?></h4>
-<h4 class="card-text text-center"><strong>Nourritture préconisée : <br> </strong> <?= isset($animal['nourriture_preconisee']) ? ucfirst($animal['nourriture_preconisee']) : 'Information non disponible'; ?></h4>
-<h4 class="card-text text-center"><strong>Quantité préconisée : <br> </strong> <?= isset($animal['grammage_preconise']) ? ucfirst($animal['grammage_preconise']) : 'Information non disponible'; ?></h4>
-<h4 class="card-text text-center"><strong>Date du rapport: <br> <?= isset($animal['date_passage']) ? date('d-m-Y', strtotime($animal['date_passage'])) : 'Date non disponible'; ?></h4>
-<h4 class="card-text text-center"><strong>Détail état : <br> </strong> <?= isset($animal['detail_etat']) ? ucfirst($animal['detail_etat']) : 'Information non disponible'; ?></h4>
-
+                    <h3 class="card-title text-center">Rapport Vétérinaire </h3>
+                    <h4 class="card-text text-center"><strong>Rapport de : <br> </strong> <?= isset($animal['user_nom_prenom']) ? ucfirst(htmlspecialchars($animal['user_nom_prenom'])) : 'Information non disponible'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Etat : <br> </strong> <?= isset($animal['etat']) ? ucfirst(htmlspecialchars($animal['etat'])) : 'Information non disponible'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Nourritture préconisée : <br> </strong> <?= isset($animal['nourriture_preconisee']) ? ucfirst(htmlspecialchars($animal['nourriture_preconisee'])) : 'Information non disponible'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Quantité préconisée : <br> </strong> <?= isset($animal['grammage_preconise']) ? ucfirst(htmlspecialchars($animal['grammage_preconise'])) : 'Information non disponible'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Date du rapport: <br> <?= isset($animal['date_passage']) ? date('d-m-Y', strtotime($animal['date_passage'])) : 'Date non disponible'; ?></h4>
+                    <h4 class="card-text text-center"><strong>Détail état : <br> </strong> <?= isset($animal['detail_etat']) ? ucfirst(htmlspecialchars($animal['detail_etat'])) : 'Information non disponible'; ?></h4>
                 </div>
             </div>
         </div>
