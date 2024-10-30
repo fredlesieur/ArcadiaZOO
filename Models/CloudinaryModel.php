17c4b5c0d969863b0cffdf02413d1203b9d27518
fredlesieur <fred.lesieur@hotmail.fr>
2024-10-30 14:16:51 +0100

    ll

--- Models/CloudinaryModel.php
+++ Models/CloudinaryModel.php
@@ -2,7 +2,6 @@
 namespace App\Models;
 
 use Cloudinary\Cloudinary;
-use Exception;
 
 class CloudinaryModel
 {
@@ -18,51 +17,24 @@ class CloudinaryModel
             ]
         ]);
     }
-    <?php
 
-    namespace App\Models;
-    
-    use Cloudinary\Cloudinary;
-    
-    class CloudinaryModel
+    public function uploadImage($imagePath)
     {
-        private $cloudinary;
-    
-        public function __construct()
-        {
-            // Initialisation Cloudinary avec les paramètres des variables d'environnement
-            $this->cloudinary = new Cloudinary([
-                'cloud' => [
-                    'cloud_name' => $_ENV['cloud_name'],
-                    'api_key'    => $_ENV['api_key'],
-                    'api_secret' => $_ENV['api_secret'],
-                ]
-            ]);
-        }
-    
-        public function uploadImage($imagePath)
-        {
+        try {
             $timestamp = time();
-            
-            // Génération de la signature pour le test
-            $signatureString = "folder=test_folder&timestamp=$timestamp";
+            $signatureString = "folder=test_folder&timestamp={$timestamp}";
             $signature = hash_hmac('sha256', $signatureString, $_ENV['api_secret']);
-            
-            try {
-                $result = $this->cloudinary->uploadApi()->upload($imagePath, [
-                    'folder' => 'test_folder',
-                    'timestamp' => $timestamp,
-                    'signature' => $signature,
-                    'api_key' => $_ENV['api_key'],
-                ]);
-    
-                return $result;
-            } catch (\Exception $e) {
-                return ['error' => $e->getMessage()];
-            }
+
+            $result = $this->cloudinary->uploadApi()->upload($imagePath, [
+                'folder' => 'test_folder',
+                'timestamp' => $timestamp,
+                'signature' => $signature,
+                'api_key' => $_ENV['api_key']
+            ]);
+
+            return $result;
+        } catch (\Exception $e) {
+            return ['error' => $e->getMessage()];
         }
     }
-    
-    
-
 }
